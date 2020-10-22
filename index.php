3a323e7ab782712bb20ae5894875c767dc470c93
Jagat Jung Lakandri B.K <64854647+Jagat-Jung@users.noreply.github.com>
2020-10-22 14:46:09 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body>
+	<div>
+			<img src="head.png" height="150" width="1100" >
+	</div>
+<ul>
+  <li><a href="home.php">Home</a></li>
+  <li><a href="product.php">Products</a></li>
+  <li><a href="login.php">Contact</a></li>
+  <li><a href="about.php">About</a></li>
+</ul>
+	<div class="main">
+		<p align="center" style="font-size:300%; color:orange;"><u>THE FACTORY</u></p>
+	 				
+		<div class="image">
+		<img class="industry" src="f1.jpg" width="780" align="center"></div>
+		<p><b>Hetauda Cement Industry</b> is one of the top cement producing industry of the Nepal. Which produces the best quality of the cement with the mines owned by the company. We here not only create the cement but the dreams of making home of our customers.We create the walls that makes our customer feel safe in their houses. The products are made with the love of our own institutions. We here not only create the cement but the dreams of making home of our customers.We create the walls that makes our customer feel safe in their houses. The products are made with the love of our own institutions.
+   			<i>"Hetauda Cement, bringing homes together"</i></p>
+	</div>
+	<div class="rightdiv"><p align="center" style="font-size:180%; color: orange;"><u><b>Workers Of The Hetauda Cement Industry(HCI)</b></u><br>
+	 								<img src="emp.jpg"  width="500" height="250">
+	 								<P  style="font-size:100%; color:white; width: 500px;text-align: justify; margin-left:0px ">
+	 							Cement contains various types of chemicals in addition to lime and silica, and such chemicals cause different health complications and pathogenesis in addition to respiratory disorders. The most important occupational hazards for cement workers are allergy and complication related to respiratory system. Even though fighting with all that dust and smoke our wokers works day and night</P>
+	 							<br><Br><br>
+	 							<img src="slogan.png"  width="400" >
+	 						</p>
+	 							<br><br><br>
+</p></div></div>
+   	
+   		</div>
+   		
+<hr>
+		<MARQUEE BGCOLOR="WHITE">THANK YOU FOR VISITING OUR SITE</MARQUEE>
+				<p class="foot">All the rights are reserved by the Hetauda Cement Industries and the website is created inorder to promote the factory and production of the company. We here, think about our customer and the company of the people Funther you can 
+			<br>contact us : 057-5279
+			<br>
+				<a href="https://www.facebook.com/?ref=tn_tnmn" target="_blank">
+						<img src="facebook.png" height="20" width="120" >
+					</a>
+					
+					<a href="https://mail.google.com/mail/u/0/#inbox" target="_blank">
+						<img src="gmail.png" height="20" width="120" >
+					</a>
+					
+					<a href="https://twitter.com/?lang=en" target="_blank"><img src="twitter.png" height="20" width="120">
+					</a><br>
+</p></body>
+</html>
+</body>
+</html>
